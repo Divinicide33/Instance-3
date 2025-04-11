fde5da470674e92bfb3ddc3e7ee2c31d88969961
Don Keiser <sworktale@gmail.com>
2025-04-11 13:43:33 +0200

    Audio Manager
    -Added Audio Manager
    -Added Scriptable SFX
    -Added SO SFX Database to store SFXs

---
+++ Instance3/Assets/Audio/Script/AudioManager.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static Action<string> OnPlaySFX;
+    public static Action<string> OnPauseSFX;
+    public static Action OnPauseAllSFX;
+    public static Action OnStopAllSFX;
+
+    public static Action<string> OnPlayMusic;
+    public static Action OnPauseMusic;
+    public static Action OnStopMusic;
+
+    [Header("Databases")]
+    [SerializeField] private SFXDatabase sfxDatabase;
+    [SerializeField] private SFXDatabase musicDatabase;
+
+    [Header("Settings")]
+    [SerializeField] private int sfxPoolSize = 6;
+
+    private AudioSource musicSource;
+    private List<AudioSource> sfxSources = new();
+    private Dictionary<string, AudioSource> activeSFX = new();
+
+    private void Awake() 
+    {
+        musicSource = gameObject.AddComponent<AudioSource>();
+        musicSource.playOnAwake = false;
+
+        for (int i = 0; i < sfxPoolSize; i++) 
+        {
+            AudioSource sfxSource = gameObject.AddComponent<AudioSource>();
+            sfxSource.playOnAwake = false;
+            sfxSources.Add(sfxSource);
+        }
+    }
+
+    private void OnEnable() 
+    {
+        OnPlaySFX += HandlePlaySFX;
+        OnPauseSFX += HandlePauseSFX;
+        OnPauseAllSFX += HandlePauseAllSFX;
+        OnStopAllSFX += HandleStopAllSFX;
+
+        OnPlayMusic += HandlePlayMusic;
+        OnPauseMusic += HandlePauseMusic;
+        OnStopMusic += HandleStopMusic;
+    }
+
+    private void OnDisable() 
+    {
+        OnPlaySFX -= HandlePlaySFX;
+        OnPauseSFX -= HandlePauseSFX;
+        OnPauseAllSFX -= HandlePauseAllSFX;
+        OnStopAllSFX -= HandleStopAllSFX;
+
+        OnPlayMusic -= HandlePlayMusic;
+        OnPauseMusic -= HandlePauseMusic;
+        OnStopMusic -= HandleStopMusic;
+    }
+
+    private void HandlePlaySFX(string sfxName) 
+    {
+        var sfxAsset = sfxDatabase.GetSFX(sfxName);
+        if (sfxAsset == null || sfxAsset.clip == null) return;
+
+        var source = GetAvaibleSFXSource();
+        if (source == null) return;
+
+        source.clip = sfxAsset.clip;
+        source.volume = sfxAsset.volume;
+        source.loop = sfxAsset.loop;
+        source.Play();
+
+        activeSFX[sfxName] = source;
+    }
+
+    private void HandlePauseSFX(string sfxName) 
+    {
+        if (activeSFX.TryGetValue(sfxName, out var source) && source.isPlaying) 
+        {
+            source.Pause();
+        }
+    }
+
+    private void HandlePauseAllSFX() 
+    {
+        foreach (var source in sfxSources) 
+        {
+            source.Stop();
+        }
+
+        activeSFX.Clear();
+    }
+
+    private void HandleStopAllSFX()
+    {
+        foreach (var source in sfxSources)
+        {
+            source.Stop();
+        }
+
+        activeSFX.Clear();
+    }
+
+    private void HandlePlayMusic(string musicName) 
+    {
+        var musicAsset = musicDatabase.GetSFX(musicName);
+        if (musicAsset == null || musicAsset.clip == null) return;
+
+        musicSource.clip = musicAsset.clip;
+        musicSource.volume = musicAsset.volume;
+        musicSource.loop = musicAsset.loop;
+        musicSource.Play();
+    }
+
+    private void HandlePauseMusic() 
+    {
+        if (musicSource.isPlaying) 
+        {
+            musicSource.Pause();
+        }
+    }
+
+    private void HandleStopMusic() 
+    {
+        if (musicSource.isPlaying) 
+        {
+            musicSource.Stop();
+        }
+    }
+
+    private AudioSource GetAvaibleSFXSource() 
+    {
+        foreach (var source in sfxSources) 
+        {
+            if (!source.isPlaying) 
+            {
+                return source;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ Instance3/Assets/Audio/Script/TestingAudio.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class TestingAudio : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            AudioManager.OnPlaySFX?.Invoke("HarpyAttack");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            AudioManager.OnPlaySFX?.Invoke("Test1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            AudioManager.OnPlaySFX?.Invoke("Test2");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            AudioManager.OnPlaySFX?.Invoke("Test3");
+        }
+    }
+}
