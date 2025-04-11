2333055a43e0d95a39a59543f41358586a2e74fa
Don Keiser <sworktale@gmail.com>
2025-04-11 15:14:01 +0200

    Audio Mixer part 2
    -Added visual for audio percentage
    -Added Reset to default Button

--- Instance3/Assets/Audio/Script/AudioSettingsManager.cs
+++ Instance3/Assets/Audio/Script/AudioSettingsManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class AudioSettingsManager : MonoBehaviour
 {
@@ -12,6 +13,14 @@ public class AudioSettingsManager : MonoBehaviour
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
 
+    [Header("Volume Labels")]
+    [SerializeField] private TMP_Text masterLabel;
+    [SerializeField] private TMP_Text musicLabel;
+    [SerializeField] private TMP_Text sfxLabel;
+
+    [Header("Reset")]
+    [SerializeField] private Button resetButton;
+
     private const string MasterParam = "Master";
     private const string MusicParam = "Music";
     private const string SFXParam = "SFX";
@@ -22,28 +31,49 @@ public class AudioSettingsManager : MonoBehaviour
 
     private void Start()
     {
-        LoadVolume(MasterParam, masterSlider, MasterKey);
-        LoadVolume(MusicParam, musicSlider, MusicKey);
-        LoadVolume(SFXParam, sfxSlider, SFXKey);
+        LoadVolume(MasterParam, masterSlider, MasterKey, masterLabel);
+        LoadVolume(MusicParam, musicSlider, MusicKey, musicLabel);
+        LoadVolume(SFXParam, sfxSlider, SFXKey, sfxLabel);
 
-        masterSlider.onValueChanged.AddListener(v => SetVolume(MasterParam, v, MasterKey));
-        musicSlider.onValueChanged.AddListener(v => SetVolume(MusicParam, v, MusicKey));
-        sfxSlider.onValueChanged.AddListener(v => SetVolume(SFXParam, v, SFXKey));
+        masterSlider.onValueChanged.AddListener(v => SetVolume(MasterParam, v, MasterKey, masterLabel));
+        musicSlider.onValueChanged.AddListener(v => SetVolume(MusicParam, v, MusicKey, musicLabel));
+        sfxSlider.onValueChanged.AddListener(v => SetVolume(SFXParam, v, SFXKey, sfxLabel));
+
+        resetButton.onClick.AddListener(ResetToDefault);
     }
 
-    private void SetVolume(string parameter, float sliderValue, string prefsKey)
+    private void SetVolume(string parameter, float sliderValue, string prefsKey, TMP_Text label)
     {
         float dB = Mathf.Log10(Mathf.Clamp(sliderValue, 0.0001f, 1f)) * 20f;
         audioMixer.SetFloat(parameter, dB);
         PlayerPrefs.SetFloat(prefsKey, sliderValue);
+        UpdateVolumeLabel(label, sliderValue);
     }
 
-    private void LoadVolume(string parameter, Slider slider, string prefsKey)
+    private void LoadVolume(string parameter, Slider slider, string prefsKey, TMP_Text label)
     {
         float savedValue = PlayerPrefs.GetFloat(prefsKey, 1f);
         slider.value = savedValue;
 
         float dB = Mathf.Log10(Mathf.Clamp(savedValue, 0.0001f, 1f)) * 20f;
         audioMixer.SetFloat(parameter, dB);
+
+        UpdateVolumeLabel(label, savedValue);
+    }
+
+    private void UpdateVolumeLabel(TMP_Text label, float sliderValue)
+    {
+        label.text = Mathf.RoundToInt(sliderValue * 100f) + "%";
+    }
+
+    private void ResetToDefault()
+    {
+        SetVolume(MasterParam, 1f, MasterKey, masterLabel);
+        SetVolume(MusicParam, 1f, MusicKey, musicLabel);
+        SetVolume(SFXParam, 1f, SFXKey, sfxLabel);
+
+        masterSlider.value = 1f;
+        musicSlider.value = 1f;
+        sfxSlider.value = 1f;
     }
 }
