8554243ee9adb3bca3b499c5922202c6eaaa85d3
kalderlunt <kalderlunt@gmail.com>
2025-04-15 12:15:55 +0200

    fix Door + doorEditor
    - door editor will get all the doors on the other stage. if there's nothing there, just rebake.

--- Instance3/Assets/Map/GestionMap/Scripts/Door.cs
+++ Instance3/Assets/Map/GestionMap/Scripts/Door.cs
@@ -1,45 +1,32 @@
 using UnityEngine;
+using System.Collections;
 
 public class Door : MonoBehaviour
 {
-    //[SerializeField] private RoomId destinationRoom;
-    //[SerializeField] private Transform destination;
-    [SerializeField] private bool goLeft = false;
-    [SerializeField] private RoomId destinationRoom;
-    [SerializeField] private Transform destinationSpawnPoint;
+    [SerializeField] private RoomId nextRoom;
+    public RoomId NextRoom => nextRoom;
 
-    private void Start()
-    {
-        if (goLeft)
-        {
-            destinationSpawnPoint.localPosition = new Vector3(-destinationSpawnPoint.localPosition.x, destinationSpawnPoint.localPosition.y, destinationSpawnPoint.localPosition.z);
-        }
-        MiniMapRoomManager.instance.RevealRoom();
-    }
+    // Nom de la porte sélectionnée dans la scène cible (renseigné via l'éditeur personnalisé)
+    [SerializeField] private string selectedDoorName;
+    public string SelectedDoorName => selectedDoorName;
 
+    // Position cible pour la téléportation dans la nouvelle scène (renseignée par le DoorEditor)
+    [SerializeField] private Vector3 targetPosition;
+    public Vector3 TargetPosition => targetPosition;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.gameObject.transform.parent.TryGetComponent<PlayerController>(out _))
             return;
-
+        
         StartCoroutine(EnterDoor(other.transform.parent));
-        MiniMapRoomManager.instance.RevealRoom();
     }
 
-    private System.Collections.IEnumerator EnterDoor(Transform player)
+    private IEnumerator EnterDoor(Transform player)
     {
-
-        yield return RoomManager.Instance.ChangeRoom(destinationRoom, player, destinationSpawnPoint.position);
+        yield return RoomManager.Instance.ChangeRoom(nextRoom, player, targetPosition);
+        
+        // on peut faire d'autres actions si nécessaire (révéler la mini-map, etc.)
+        //MiniMapRoomManager.instance.RevealRoom();
     }
-
-    //private void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    
-    //    if (playerController != null)
-    //    {
-    //        PlayerInputScript.onDisableInput?.Invoke();
-    //        Transform player = playerController.transform;
-    //        
-    //    }
-    //}
 }
