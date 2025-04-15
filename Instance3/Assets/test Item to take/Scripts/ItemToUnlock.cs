c95cc3dac91db5e7b0198bcadd49e495d7bd9bb2
Gabibel15022005 <153726789+Gabibel15022005@users.noreply.github.com>
2025-04-15 17:45:21 +0200

    Added test
    Moved some script
    
    Added prefab enemy test
    Added prefab item test and skill test

---
+++ Instance3/Assets/test Item to take/Scripts/ItemToUnlock.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ItemToUnlock : MonoBehaviour
+{
+    [SerializeField] private ItemsName itemName;
+
+    void Start()
+    {
+        // For debug
+        PlayerPrefs.SetInt(itemName.ToString(), 0);
+        PlayerPrefs.Save();
+
+        if (PlayerPrefs.HasKey(itemName.ToString()) && PlayerPrefs.GetInt(itemName.ToString()) == 1) 
+        gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer(LayerMap.Player.ToString()))
+        {
+            PlayerSkillManager.onSetItem?.Invoke(itemName, true);
+            PlayerPrefs.SetInt(itemName.ToString(), 1);
+            PlayerPrefs.Save();
+            gameObject.SetActive(false);
+        }
+    }
+}
