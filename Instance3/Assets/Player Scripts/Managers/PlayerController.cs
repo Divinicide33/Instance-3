d9164a00ca7672ae07020c5a839921d537c03ef3
Gabibel15022005 <153726789+Gabibel15022005@users.noreply.github.com>
2025-04-15 18:17:28 +0200

    Update PlayerController.cs
    minor change

--- Instance3/Assets/Player Scripts/Managers/PlayerController.cs
+++ Instance3/Assets/Player Scripts/Managers/PlayerController.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class PlayerController : Entity
 {
     private Stats stats;
-    private PlayerMove move;
-    private PlayerJump jump;
-    private PlayerAttack attack;
-    private PlayerDash dash;
-    private PlayerGlide glide;
 
     public static Action<Vector2> onMove { get; set; }
     public static Action<bool> onJump { get; set; }
@@ -27,11 +22,6 @@ public class PlayerController : Entity
     void Start()
     {
         stats = GetComponent<Stats>();
-        move = GetComponent<PlayerMove>();
-        jump = GetComponent<PlayerJump>();
-        attack = GetComponent<PlayerAttack>();
-        dash = GetComponent<PlayerDash>();
-        glide = GetComponent<PlayerGlide>();
 
         UpdatePlayerUi();
 
